277eabf1ae8c9e7ea8f9bc0a2a9bc6dbe1ddd2c5
Hansel Ke <hansel@openbusiness.com.sg>
2015-09-14 23:29:06 +0800

    add demo data

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 apt-get update && apt-get install -y wget ca-certificates sudo cron supervisor
 /usr/bin/supervisord
 
-wget https://raw.githubusercontent.com/frappe/bench/master/install_scripts/setup_frappe.sh
 bash setup_frappe.sh --setup-production
 rm /home/frappe/*.deb
 
